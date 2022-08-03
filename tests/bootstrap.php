e497692dd8d48ff964646463b9c0a9f85fe0210e
Sean Fisher <srtfisher@gmail.com>
2022-08-03 13:08:52 -0400

    phpcs fixes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * Initialize the plugin for testing.
+ *
+ * @package Archiveless
  */
 
 \Mantle\Testing\manager()
--- tests/test-general.php
+++ tests/test-general.php
@@ -1,8 +1,16 @@
 <?php
+/**
+ * General test file
+ *
+ * @package Archiveless
+ */
 
 use Mantle\Testing\Concerns\Refresh_Database;
 use Mantle\Testkit\Test_Case;
 
+/**
+ * General Test Case
+ */
 class Test_General extends Test_Case {
 	use Refresh_Database;
 
@@ -13,26 +21,46 @@ class Test_General extends Test_Case {
 	protected function setUp(): void {
 		parent::setUp();
 
-		$category_id = $this->factory->term->create( array(
-			'taxonomy' => 'category',
-			'name' => 'archives',
-		) );
-
-		$author_id = $this->factory->user->create( array(
-			'role'        => 'author',
-			'user_login'  => 'test_author',
-			'description' => 'test_author',
-		) );
-
-		$defaults = array(
-			'post_date'     => '2015-01-01 00:00:01',
-			'post_category' => array( $category_id ),
-			'post_author'   => $author_id,
-			'post_content'  => 'Lorem ipsum',
+		$category_id = $this->factory->term->create(
+			[
+				'taxonomy' => 'category',
+				'name'     => 'archives',
+			]
 		);
 
-		$this->archiveless_post = $this->factory->post->create( array_merge( $defaults, array( 'post_title' => 'archiveless post', 'post_status' => 'archiveless' ) ) );
-		$this->archiveable_post = $this->factory->post->create( array_merge( $defaults, array( 'post_title' => 'archiveable post', 'post_status' => 'publish' ) ) );
+		$author_id = $this->factory->user->create(
+			[
+				'role'        => 'author',
+				'user_login'  => 'test_author',
+				'description' => 'test_author',
+			]
+		);
+
+		$defaults = [
+			'post_date'     => '2015-01-01 00:00:01',
+			'post_category' => [ $category_id ],
+			'post_author'   => $author_id,
+			'post_content'  => 'Lorem ipsum',
+		];
+
+		$this->archiveless_post = $this->factory->post->create(
+			array_merge(
+				$defaults,
+				[
+					'post_title'  => 'archiveless post',
+					'post_status' => 'archiveless',
+				]
+			)
+		);
+		$this->archiveable_post = $this->factory->post->create(
+			array_merge(
+				$defaults,
+				[
+					'post_title'  => 'archiveable post',
+					'post_status' => 'publish',
+				]
+			)
+		);
 	}
 
 	public function test_verify_post_status_exists() {
@@ -46,6 +74,8 @@ class Test_General extends Test_Case {
 	}
 
 	/**
+	 * Test that an archiveless post is not accessible under multiple conditions.
+	 *
 	 * @dataProvider inaccessible
 	 */
 	public function test_inaccessible( $url, $conditional ) {
@@ -59,37 +89,41 @@ class Test_General extends Test_Case {
 	}
 
 	public function inaccessible() {
-		return array(
-			array( '/', 'is_home' ), // Homepage
-			array( '/?year=2015&monthnum=01', 'is_date' ), // Date archive
-			array( '/?category_name=archives', 'is_category' ), // Tax archive
-			array( '/?author_name=test_author', 'is_author' ), // Author archive
-			array( '/?s=Lorem+ipsum', 'is_search' ), // Search
-			array( '/?feed=rss', 'is_feed' ), // Feeds
-		);
+		return [
+			[ '/', 'is_home' ], // Homepage.
+			[ '/?year=2015&monthnum=01', 'is_date' ], // Date archive.
+			[ '/?category_name=archives', 'is_category' ], // Tax archive.
+			[ '/?author_name=test_author', 'is_author' ], // Author archive.
+			[ '/?s=Lorem+ipsum', 'is_search' ], // Search.
+			[ '/?feed=rss', 'is_feed' ], // Feeds.
+		];
 	}
 
 	public function test_future_post_transition() {
-		// Create a future post and add the archiveless post meta value
-		$post_id = $this->factory->post->create( array(
-			'post_title' => 'future archiveless post',
-			'post_date' => gmdate( 'Y-m-d H:i:s', time() + DAY_IN_SECONDS ),
-			'post_status' => 'future',
-		) );
+		// Create a future post and add the archiveless post meta value.
+		$post_id = $this->factory->post->create(
+			[
+				'post_title'  => 'future archiveless post',
+				'post_date'   => gmdate( 'Y-m-d H:i:s', time() + DAY_IN_SECONDS ),
+				'post_status' => 'future',
+			]
+		);
 		add_post_meta( $post_id, 'archiveless', '1' );
 
-		// Verify that the post correctly inserted with the 'future' status
+		// Verify that the post correctly inserted with the 'future' status.
 		$this->assertEquals( 'future', get_post_status( $post_id ) );
 
-		// Set a new date in the past
+		// Set a new date in the past.
 		$new_date = gmdate( 'Y-m-d H:i:s', time() - DAY_IN_SECONDS );
-		wp_update_post( array(
-			'ID' => $post_id,
-			'post_date' => $new_date,
-			'post_date_gmt' => get_gmt_from_date( $new_date ),
-		) );
+		wp_update_post(
+			[
+				'ID'            => $post_id,
+				'post_date'     => $new_date,
+				'post_date_gmt' => get_gmt_from_date( $new_date ),
+			]
+		);
 
-		// Verify that the post transitioned to 'archiveless' (due to the post meta)
+		// Verify that the post transitioned to 'archiveless' (due to the post meta).
 		$this->assertEquals( 'archiveless', get_post_status( $post_id ) );
 	}
 
@@ -99,10 +133,12 @@ class Test_General extends Test_Case {
 	 */
 	public function test_post_meta_hooks() {
 		// Create a post.
-		$post_id = $this->factory->post->create( array(
-			'post_title' => 'Test Archiveless Post',
-			'post_status' => 'publish',
-		) );
+		$post_id = $this->factory->post->create(
+			[
+				'post_title'  => 'Test Archiveless Post',
+				'post_status' => 'publish',
+			]
+		);
 
 		// Verify that the post status is 'publish'.
 		$this->assertEquals( 'publish', get_post_status( $post_id ) );
