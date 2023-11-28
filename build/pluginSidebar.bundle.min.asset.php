7b74e75976c5b787a0450c1130d6d2dc325bd3d5
srtfisher <srtfisher@users.noreply.github.com>
2023-11-28 20:21:50 +0000

    Changes from 049e6cc5ec0f263596ad2595059d3ff057fadf19

---
+++ build/pluginSidebar.bundle.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '6cc5630e3f8ea584866b');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alleyinteractive/archiveless',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alleyinteractive/archiveless' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
