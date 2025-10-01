1f784a0f2800b17fd63b9b25d543ecc13952dbe0
Sean Fisher <srtfisher@gmail.com>
2025-10-01 14:43:26 -0400

    Contd work

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,12 +6,5 @@
  */
 
 \Mantle\Testing\manager()
-	->loaded(
-		function () {
-			require_once __DIR__ . '/../archiveless.php';
-
-			// Set the permalink structure.
-			update_option( 'permalink_structure', '/%year%/%monthnum%/%day%/%postname%/' );
-		} 
-	)
+	->loaded( fn () => require_once __DIR__ . '/../archiveless.php' )
 	->install();
