d7bce4abdbf61eac51d30d104e77363c34e70a7b
srtfisher <srtfisher@users.noreply.github.com>
2025-10-22 20:53:27 +0000

    Built changes for v1.2.0 from main c02ac7845766474e0313cd23f840c93248400d8a

---
+++ build/sidebar/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => '7e6e73f42c662d3540a6');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alleyinteractive/archiveless',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c02ac7845766474e0313cd23f840c93248400d8a',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alleyinteractive/archiveless' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c02ac7845766474e0313cd23f840c93248400d8a',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
