a0f449f2afee8f9cba2e17af996284a07e668880
samhermes <samhermes@users.noreply.github.com>
2026-09-23 18:27:46 +0000

    Built changes from main 3e808860ef6fd1deea678ec55f13cc5a504c2aad

---
+++ build/sidebar/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => '7fe3c686ac7442390711');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alleyinteractive/archiveless',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3e808860ef6fd1deea678ec55f13cc5a504c2aad',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alleyinteractive/archiveless' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3e808860ef6fd1deea678ec55f13cc5a504c2aad',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
